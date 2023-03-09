70b55c244fb7f2a29d191ff6cf7cd6048162f7b2
danwoo <danwoo@lbox.kr>
2023-03-09 10:52:11 +0900

    Controller 계층을 Kotlin으로 변경하기

--- src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -26,7 +26,7 @@ class UserService(
     @Transactional(readOnly = true)
     fun getUsers(): List<UserResponse>{
         return userRepository.findAll().map {
-            UserResponse(it)
+            UserResponse.of(it)
         }
     }
 
