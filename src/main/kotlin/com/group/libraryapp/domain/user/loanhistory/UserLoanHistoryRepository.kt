ccca36aa55a6014e5e34a6248ef347939486eee0
danwoo <danwoo@lbox.kr>
2023-03-12 13:03:04 +0900

    책 통계 보여주기 - 프로덕션 코드 개발

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
     fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
     // TODO: 옵셔널처리 안하면 null이 될수도 있어서 ? 붙여줘야함
+    fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
 }
